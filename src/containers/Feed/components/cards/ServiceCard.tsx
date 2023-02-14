e00d20f4e44e5e0abeab951d5682dba876157f0e
AmeloYi <ly595788168@hotmail.com>
2023-02-14 16:31:58 +0800

    update

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -8,10 +8,10 @@ const ServiceCard: React.FC = () => {
     <>
       <div className="p-1 mb-3 dark:text-white">🌟 Service</div>
       {
-        CONFIG.projects.map((item, idx) => (
           <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
+                  CONFIG.projects.map((item, idx) => (
         <a
-          href={`{item.href}`}
+          href={`${item.href}`}
           rel="noreferrer"
           target="_blank"
           className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
@@ -19,8 +19,8 @@ const ServiceCard: React.FC = () => {
           <AiFillCodeSandboxCircle className="text-2xl" />
           <div className="text-sm">{item.name}</div>
         </a>
+                  ))
       </ul>
-          ))
       }
       <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
         <a

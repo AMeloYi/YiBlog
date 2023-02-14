1bae6168f9b43ca80a14a5994f7f789ff06e7688
AmeloYi <ly595788168@hotmail.com>
2023-02-14 16:45:46 +0800

    update

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -30,7 +30,7 @@ const ContactCard: React.FC = () => {
             target="_blank"
             className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
           >
-            <AiOutlineInstagram className="text-2xl" />
+            <AiOutlineTwitter className="text-2xl" />
             <div className="text-sm">twitter</div>
           </a>
         )}
